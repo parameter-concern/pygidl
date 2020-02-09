c2436ce13ac6eaf52ad943f6d7c584c382710b90
parameter-concern <60160508+parameter-concern@users.noreply.github.com>
2020-02-08 19:26:49 -0500

    1 update gi parser (#2)
    * Add parser for new google images metadata format
    
    * Remove html debug code

--- src/pygidl/google_images.py
+++ src/pygidl/google_images.py
@@ -2,6 +2,7 @@
 import asyncio
 import json
 import logging
+import re
 from typing import List
 
 import aiohttp
@@ -54,9 +55,7 @@ async def search_worker(
             response_text = ""
         logger.debug(f"done querying google for '{query}', parsing")
         found_images = parse_google_images_results(response_text, query)
-        logger.info(
-            "found %d images querying for %s", len(found_images), query
-        )
+        logger.info("found %d images querying for %s", len(found_images), query)
         for image_metadata in found_images:
             # logger.debug("putting %s onto download queue", result.url)
             await download_queue.put(image_metadata)
@@ -69,23 +68,69 @@ def parse_google_images_results(
 ) -> List[utils.ImageMetadata]:
     LOG.debug("parsing Google Images results")
     dom = bs4.BeautifulSoup(html, "html.parser")
+    for parser in [parse_results_type_a, parse_results_type_b]:
+        results = parser(dom, query)
+        if results:
+            return results
+    return []
+
+
+def parse_results_type_a(
+    dom: bs4.BeautifulSoup, query: utils.GoogleImagesQuery
+) -> List[utils.ImageMetadata]:
+    """Parse the prominent Google Images page data format as of 20200101."""
     results = []
     for result_div in dom.select("div.rg_meta.notranslate"):
-        results.append(parse_google_images_result(result_div.text, query))
+        metadata = json.loads(result_div.text)
+        results.append(
+            utils.ImageMetadata(
+                url=metadata["ou"],
+                image_format=metadata["ity"],
+                description=metadata["pt"],
+                height=metadata["oh"],
+                width=metadata["ow"],
+                host=metadata["rh"],
+                source=metadata["ru"],
+                query=query,
+            )
+        )
     return results
 
 
-def parse_google_images_result(
-    result_text: str, query: utils.GoogleImagesQuery
-) -> utils.ImageMetadata:
-    metadata = json.loads(result_text)
-    return utils.ImageMetadata(
-        url=metadata["ou"],
-        image_format=metadata["ity"],
-        description=metadata["pt"],
-        height=metadata["oh"],
-        width=metadata["ow"],
-        host=metadata["rh"],
-        source=metadata["ru"],
-        query=query,
-    )
+def parse_results_type_b(
+    dom: bs4.BeautifulSoup, query: utils.GoogleImagesQuery
+) -> List[utils.ImageMetadata]:
+    """Parse the prominent Google Images page data format as of 20200201."""
+    results = []
+    for script in dom.select("script"):
+        script_text = script.get_text()
+        script_text = script_text.replace("\n", " ")
+        match = re.search(
+            r"AF_initDataCallback\((.*?)return(.*)}}\);", script_text
+        )  # reliable? looks like a huge blob of callback args
+        data = None
+        if match:
+            data = json.loads(match.group(2))
+            if len(data) < 2:
+                continue
+        else:
+            continue
+        for i, d in enumerate(data[31][0][12][2]):  # reliable? seems to work.
+            try:
+                url, height, width = d[1][3]
+                results.append(
+                    utils.ImageMetadata(
+                        url=url,
+                        image_format=None,
+                        description=None,
+                        height=height,
+                        width=width,
+                        host=None,
+                        source=None,
+                        query=query,
+                    )
+                )
+            except TypeError:
+                continue
+    return results
+
